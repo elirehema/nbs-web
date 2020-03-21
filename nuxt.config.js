41782049c06d78f79ffa4e2845fb7ad928455d78
=elirehema <=rocksandystone@gmail.com>
2020-03-21 13:38:36 +0300

    Changed Base Url

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,12 +6,7 @@ export default {
     base: process.env.NODE_ENV === 'production' ? '/nbs-web/' : '/',
     routerNameSplitter: '/',
   },
-  /**
-  * Dotenv enviroment
-  **/
-  env: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:8080/api/'
-  },
+
   /*
    ** Headers of the page
    */
