bc81b4d596278f5552c246e2e3d12edf99d4ac5c
=elirehema <=rocksandystone@gmail.com>
2020-03-21 13:15:12 +0300

    Finalize Listing

--- plugins/axios.js
+++ plugins/axios.js
@@ -28,7 +28,7 @@ export default function ({
   });
 
   // Set baseURL to something different
-  api.setBaseURL(process.env.baseUrl);
+  api.setBaseURL('https://nbsapi.herokuapp.com/');
 
   // Inject to context as $api
   inject('api', api);
