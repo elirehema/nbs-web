fb71070d46e0bfad7868275af630baa8c16e90c6
=elirehema <=rocksandystone@gmail.com>
2020-03-30 19:02:51 +0300

    Remove Navigation Drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,57 +1,5 @@
 <template>
   <v-app class="app">
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
-      <v-card class="mx-auto" flat>
-        <v-list-item>
-          <v-router-link to="/profile">
-            <v-list-item-avatar>
-              <v-img src="https://answersafrica.com/wp-content/uploads/2015/01/somalia.jpg" dark></v-img>
-            </v-list-item-avatar>
-          </v-router-link>
-          <v-list-item-content>
-            <v-list-item-title class="headline">@{{userdata.username}}</v-list-item-title>
-            <v-list-item-subtitle>{{userdata.email}}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-card-actions class="info lighten-1">
-          <v-btn dark icon @click="drawer = !drawer">
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-          <v-btn dark icon @click="changemode">
-            <v-icon v-if="!dark">mdi-brightness-6</v-icon>
-            <v-icon v-if="dark">mdi-brightness-5</v-icon>
-          </v-btn>
-
-          <v-menu bottom left>
-            <template v-slot:activator="{ on }">
-              <v-btn dark icon v-on="on">
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
-            </template>
-
-            <v-list dense>
-              <v-list-item v-for="(item, i) in actions" :key="i" @click="selectedItemAction(i)">
-                <v-list-item-avatar width="36" height="35">
-                  <v-icon class="success white--text" v-text="item.icon"></v-icon>
-                </v-list-item-avatar>
-                <v-list-item-title class="font-weight-light">{{ item.title }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-        </v-card-actions>
-      </v-card>
-      <v-list subheader tile>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-          <v-list-item-avatar width="36" height="35">
-            <v-icon :class="[item.iconClass]" v-text="item.icon"></v-icon>
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title class="font-weight-light">{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
       elevation="0"
       :clipped-left="clipped"
@@ -59,43 +7,37 @@
       fixed
       app
       dark
-      src="https://www.tanzaniatourism.go.tz/images/featured/mount-kilimanjaro.jpg"
+      src="https://ak1.picdn.net/shutterstock/videos/986371/thumb/1.jpg"
       fade-img-on-scroll
       shrink-on-scroll
       prominent
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-
       <v-toolbar-title>Tanzania National Bureau of Statistics</v-toolbar-title>
 
       <v-spacer></v-spacer>
-      <!--<template v-slot:extension>
-        <v-tabs horizontal hide-slider center-active>
-          <v-tab color="indigo" centered:true v-for="(item, index) in tabs" :key="index">
-            <p class="overline font-weight-medium">{{item.title}}</p>
-          </v-tab>
-          <v-tab-item>
-            <v-card text>
-              <news-component></news-component>
-            </v-card>
-          </v-tab-item>
-          <v-tab-item>
-            <indicators-component></indicators-component>
-          </v-tab-item>
-          <v-tab-item>
-            <sectors-component></sectors-component>
-          </v-tab-item>
-          <v-tab-item>
-            <indicatorsource-component></indicatorsource-component>
-          </v-tab-item>
-          <v-tab-item>
-            <currentrelease-component></currentrelease-component>
-          </v-tab-item>
-          <v-tab-item>
-            <disaggregations-component></disaggregations-component>
-          </v-tab-item>
-        </v-tabs>
-      </template>-->
+
+      <v-menu bottom left>
+        <template v-slot:activator="{ on }">
+          <div class="text-center">
+            <v-btn v-on="on" class="mx-2" fab dark small color="warning">
+              <v-icon dark>mdi-power</v-icon>
+            </v-btn>
+          </div>
+
+          <!-- <v-btn dark icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>-->
+        </template>
+
+        <v-list dense>
+          <v-list-item v-for="(item, i) in actions" :key="i" @click="selectedItemAction(i)">
+            <v-list-item-avatar width="36" height="35">
+              <v-icon class="success white--text" v-text="item.icon"></v-icon>
+            </v-list-item-avatar>
+            <v-list-item-title class="font-weight-light">{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
 
     <v-content>
@@ -194,7 +136,7 @@ export default {
         }
       ],
       actions: [
-        { title: "View profile", icon: "mdi-account-circle" },
+        // { title: "View profile", icon: "mdi-account-circle" },
         { title: "Logout", icon: "mdi-logout-variant" }
       ],
       miniVariant: false,
